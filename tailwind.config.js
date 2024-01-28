e170be47cfc0d124e2722b0d06ba692d8aca24d7
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-01-28 17:04:08 +0700

    install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    // Remove the Tailwind CSS preflight styles so it can use Material UI's preflight instead (CssBaseline).
+    preflight: false,
+  },
+  plugins: [],
+}
+
