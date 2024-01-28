987610ca861acfbb512e66c5715ac4e68b812026
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-01-28 22:31:00 +0700

    refactor: extract section items to component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import "./App.css";
 
 function Avatar() {
@@ -15,11 +16,23 @@ function About() {
     <>
       <h2 className="col-span-full">About</h2>
       <p className="col-span-full">
-        Hey, I&apos;m Pete. I started my journey as a software engineer in
-        2023. Craving learning and focused on developing impactful experiences
-        that constantly improve based on user feedback. One percent better
-        everyday.
+        Hey, I&apos;m Pete. I started my journey as a software engineer in 2023.
+        Craving learning and focused on developing impactful experiences that
+        constantly improve based on user feedback. One percent better everyday.
       </p>
+    </>
+  );
+}
+
+function SectionItem({ title, date, description, skills }) {
+  return (
+    <>
+      <span className="col-span-full sm:col-span-1">{date}</span>
+      <div className="col-span-full sm:col-span-1 flex flex-col mb-4 sm:mb-0">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <span>{skills}</span>
+      </div>
     </>
   );
 }
@@ -29,18 +42,12 @@ function Experience() {
     <>
       <h2 className="col-span-full">Experience</h2>
 
-      <span className="col-span-full sm:col-span-1">Nov 2023 - Feb 2024</span>
-      <div className="col-span-full sm:col-span-1 flex flex-col mb-4 sm:mb-0">
-        <h3>Junior Software Developer Bootcamp - Generation Thailand</h3>
-        <p>
-          Developed technical skills in building web applications. Gained
-          essential soft skills and mindsets, and learned about effective
-          communication and agile methodologies.
-        </p>
-        <span>
-          React • Node.js • Express.js • MongoDB • CSS • Tailwind CSS • SQL
-        </span>
-      </div>
+      <SectionItem
+        title="Junior Software Developer Bootcamp - Generation Thailand"
+        date="Nov 2023 - Feb 2024"
+        description="Developed technical skills in building web applications. Gained essential soft skills and mindsets, and learned about effective communication and agile methodologies."
+        skills="React • Node.js • Express.js • MongoDB • CSS • Tailwind CSS • SQL"
+      />
 
       <span className="col-span-full sm:col-span-1">Earlier Experience</span>
       <div className="col-span-full sm:col-span-1 flex flex-col mb-4 sm:mb-0">
@@ -59,21 +66,19 @@ function Projects() {
     <>
       <h2 className="col-span-full">Projects</h2>
 
-      <span className="col-span-full sm:col-span-1">Ongoing</span>
-      <div className="col-span-full sm:col-span-1 flex flex-col mb-4 sm:mb-0">
-        <h3>Practice Buddy</h3>
-        <p>A web app designed for musicians to keep track of their repertoire</p>
-        <span>MUI • React • Node.js • Express.js • MongoDB</span>
-      </div>
-
-      <span className="col-span-full sm:col-span-1">Jan - Feb 2024</span>
-      <div className="col-span-full sm:col-span-1 flex flex-col mb-4 sm:mb-0">
-        <h3>LogLife - Bootcamp Project</h3>
-        <p>
-          An exercise tracking web app aimed at beginners with no smartwatches
-        </p>
-        <span>Tailwind CSS • React • Node.js • Express.js • MongoDB</span>
-      </div>
+      <SectionItem
+        title="Practice Buddy"
+        date="Ongoing"
+        description="A web app designed for musicians to keep track of their repertoire"
+        skills="MUI • React • Node.js • Express.js • MongoDB"
+      />
+      
+      <SectionItem
+        title="LogLife - Bootcamp Project"
+        date="Jan - Feb 2024"
+        description="An exercise tracking web app aimed at beginners with no smartwatches"
+        skills="Tailwind CSS • React • Node.js • Express.js • MongoDB"
+      />
     </>
   );
 }
