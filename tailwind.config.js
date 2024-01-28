56da24f60cc1ad5391235cfdf35ee9d6364949c1
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-01-28 19:37:12 +0700

    build: reinstate tailwind preflight style

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,6 @@ export default {
   theme: {
     extend: {},
   },
-  corePlugins: {
-    // Remove the Tailwind CSS preflight styles so it can use Material UI's preflight instead (CssBaseline).
-    preflight: false,
-  },
   plugins: [],
 }
 
